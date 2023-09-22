211d2d250e4e4f91453175adfc0a0851151e72dd
molano5858 <molano5858@gmail.com>
2023-09-22 11:06:15 -0500

    Creation navbar and hero section responsive with styles.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,42 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../Assets/Images/logo Lemon yellow horizontal.png";
 
 function Nav() {
+  const [mostarBurguerMenu, setMostrarBurguerMenu] = useState(false);
+
+  const handlerIcon = () => {
+    console.log("hola");
+    setMostrarBurguerMenu(!mostarBurguerMenu);
+  };
   return (
     <>
-      <ul className="nav-list">
-        <img className="nav-logo" src={logo} alt="Little lemon logo" />
-        <li className="nav-listItem">
-          <a className="nav-link" href="/home">
+      <nav className="nav-bar">
+        <div
+          className={`burger-menu ${
+            !mostarBurguerMenu ? "showBurguerMenu" : "hideBurguerMenu"
+          }`}
+          id="burger-menu"
+        >
+          <a className="nav-link-burguer" href="#home">
             Home
           </a>
-        </li>
-        <li className="nav-listItem">
-          <a className="nav-link" href="/about">
+          <a className="nav-link-burguer" href="#about">
             About Us
           </a>
-        </li>
-        <li className="nav-listItem">
-          <a className="nav-link" href="/menu">
+          <a className="nav-link-burguer" href="#menu">
             Menú
           </a>
-        </li>
-        <li className="nav-listItem">
-          <a className="nav-link" href="/reservas">
+          <a className="nav-link-burguer" href="#menu">
             Reservas
           </a>
-        </li>
-        <li className="nav-listItem">
-          <a className="nav-link" href="/blog">
+          <a className="nav-link-burguer" href="#menu">
             Pedidos en linea
           </a>
-        </li>
-        <li className="nav-listItem">
-          <a className="nav-link" href="/login">
+          <a className="nav-link-burguer" href="#menu">
             Log In
           </a>
-        </li>
-      </ul>
+        </div>
+        <img className="nav-logo" src={logo} alt="Little lemon logo" />
+        <ul className="nav-list">
+          <li className="nav-listItem">
+            <a className="nav-link" href="/home">
+              Home
+            </a>
+          </li>
+          <li className="nav-listItem">
+            <a className="nav-link" href="/about">
+              About Us
+            </a>
+          </li>
+          <li className="nav-listItem">
+            <a className="nav-link" href="/menu">
+              Menú
+            </a>
+          </li>
+          <li className="nav-listItem">
+            <a className="nav-link" href="/reservas">
+              Reservas
+            </a>
+          </li>
+          <li className="nav-listItem">
+            <a className="nav-link" href="/blog">
+              Pedidos en linea
+            </a>
+          </li>
+          <li className="nav-listItem">
+            <a className="nav-link" href="/login">
+              Log In
+            </a>
+          </li>
+        </ul>
+
+        <div onClick={handlerIcon} className={`icons `} id="icons">
+          <i
+            className={`bx bx-menu ${
+              mostarBurguerMenu ? "showIcon" : "hideIcon"
+            }`}
+            id="menuIcon"
+          ></i>
+          <i
+            className={`bx bx-x ${
+              !mostarBurguerMenu ? "showIcon" : "hideIcon"
+            }`}
+            id="menuX"
+          ></i>
+        </div>
+      </nav>
+      {}
     </>
   );
 }
